61eed62e8db74523b84950d968cd2eb906e3df55
Griefed <griefed@griefed.de>
2021-07-13 16:31:31 +0200

    refactor: Apply Griefed.de style to Error404 page

--- src/pages/Error404.vue
+++ src/pages/Error404.vue
@@ -1,12 +1,12 @@
 <template>
-  <div class="fullscreen bg-blue text-white text-center q-pa-md flex flex-center">
+  <div class="background text-white text-center q-pa-md flex flex-center">
     <div>
       <div style="font-size: 30vh">
         404
       </div>
 
       <div class="text-h2" style="opacity:.4">
-        Oops. Nothing here...
+        You shouldn't be here...
       </div>
 
       <q-btn
@@ -23,9 +23,19 @@
 </template>
 
 <script>
-import { defineComponent } from 'vue'
-
-export default defineComponent({
-  name: 'Error404'
-})
+export default {
+  name: 'Error404',
+  data() {
+    return {}
+  }
+}
 </script>
+
+<style>
+.background {
+  background-image: url("~assets/background.webp");
+  background-repeat: repeat;
+  background-attachment: fixed;
+}
+
+</style>
